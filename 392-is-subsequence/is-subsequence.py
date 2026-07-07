48376d19528451d57f230559acf1c9c971172386
Nihar Kikani <nihar.k1@ahduni.edu.in>
2026-07-07 21:12:29 +0530

    Time: 3 ms (24.50%) | Memory: 19.1 MB (91.57%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+        # If characters match, move the pointer of s forward
+            if s[i] == t[j]:
+                i += 1
+        # Always move the pointer of t forward
+            j += 1
+    
+        # If we have traversed all characters of s, return True
+        return i == len(s)
+            
+        
